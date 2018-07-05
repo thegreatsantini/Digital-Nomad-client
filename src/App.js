79ba1b122dca1690e4e11952c36b2e058f5078a2
thegreatsantini <lss13@my.fsu.edu>
2018-07-05 16:50:49 -0700

    initial app.js functionality, working on auth

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { SERVER_URL } from 'constants';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Nav from './layout/Nav'
+import Login from './auth/Login';
+import SignUp from './auth/SignUp';
+import Home from './Home';
+import User from './User'
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    }
+  }
+
+  componentDidMount = () => {
+    this.getUser();
+  }
+
+  getUser = () => {
+    // get user
+    let token = localStorage.getItem('loginToken');
+    if (token) {
+      // there is a token in localStorage; validate it
+      axios.post(SERVER_URL + '/auth/me/from/token', {
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+        .then(response => {
+          this.setState({
+            user: response.data.user
+          });
+        })
+        .catch(err => {
+          console.log('error:', err);
+          localStorage.removeItem('loginToken');
+          this.setState({
+            user: null
+          })
+        });
+    }
+    else {
+      console.log('No token was found');
+      localStorage.removeItem('loginToken');
+      this.setState({
+        user: null
+      })
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <div className="container">
+              <Nav user={this.state.user} updateUser={this.getUser} />
+
+              <Route path="/user" component={User} />
+              <Route path="/login" component={() => (<Login user={this.state.user} updateUser={this.getUser} />)} />
+              <Route path="/signup" component={() => (<SignUp user={this.state.user} updateUser={this.getUser} />)} />
+              <Route exact path='/' component={() => (<Home user={this.state.user} updateUser={this.getUser}  />)} />
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
