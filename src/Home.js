95c098356a805e1db4a85fcadf3a6df044695a8b
thegreatsantini <lss13@my.fsu.edu>
2018-07-24 12:12:03 -0700

    added tabs and addressbook component

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
+import AddressBook from './AddressBook'
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
+    constructor(props, context) {
+        super(props, context);
+
+        this.handleSelect = this.handleSelect.bind(this);
+
+        this.state = {
+            key: 1
+        };
+    }
+
+    componentDidMount = () => {
+        console.log( 'Home comp:', this.props.user)
+    };
+
+    handleSelect(key) {
+
+        this.setState({ key });
     }
 
     render() {
         return (
             <div>
-                This is the home page
-                {this.props.user ? this.props.user.name : "Login please"}
-                
+                <Tabs
+                    activeKey={this.state.key}
+                    onSelect={this.handleSelect}
+                    id="controlled-tab-example"
+                >
+                    <Tab eventKey={1} title="Address Book">
+                        <AddressBook user={this.props.user} />
+                    </Tab>
+                    <Tab eventKey={2} title="Send post card">
+                        send card here {this.props.user}
+                    </Tab>
+                    <Tab eventKey={3} title="Settings">
+                        Settings
+                    </Tab>
+                </Tabs>
             </div>
         );
     }
