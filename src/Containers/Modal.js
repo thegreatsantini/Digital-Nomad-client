a663136ea607cd165612609028f7610f7808f76c
thegreatsantini <lss13@my.fsu.edu>
2018-08-13 22:32:20 -0700

    added modal component

---
+++ src/Containers/Modal.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { ButtonToolbar, Button, Modal } from 'react-bootstrap';
+
+export default class ModalComp extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+
+    this.state = {
+      show: false
+    };
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+
+  handleHide() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    return (
+      <ButtonToolbar>
+        <Button bsStyle="primary" onClick={this.handleShow}>
+          Info
+        </Button>
+
+        <Modal
+          {...this.props}
+          show={this.state.show}
+          onHide={this.handleHide}
+          dialogClassName="custom-modal"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-lg">
+              { this.props.contacts }
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Wrapped Text</h4>
+            <p>
+              {this.props.message}
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </ButtonToolbar>
+    );
+  }
+}
