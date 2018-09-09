d1a41d169a21e231f1ea032de707d01a6c909a94
thegreatsantini <lss13@my.fsu.edu>
2018-09-09 15:09:57 -0700

    gave profile child props

--- src/Routes.js
+++ src/Routes.js
@@ -5,7 +5,7 @@ import SignUp from './auth/SignUp';
 import Home from './Components/Home';
 import AddressBook from './Components/AddressBook';
 import Profile from './Components/Profile';
-import EditContactForm from './Forms/EditContactForm';
+import EditContactForm from './Components/EditContactForm';
 import SandBox from './SandBox';
 import AppliedRoute from './Components/AppliedRoute';
 import NotFound from './Containers/NotFound';
@@ -16,12 +16,11 @@ export default ({ childProps }) =>
     <Switch>
         <AppliedRoute path="/" exact component={Home} props={childProps}  />
         
-
+        <AppliedRoute path="/profile" component={Profile} props={childProps} />
         <AppliedRoute path="/login" component={Login} props={childProps} />
         <AppliedRoute path="/signup" component={SignUp} props={childProps} />
         
         <AppliedRoute path="/contacts/edit/" component={() => (<EditContactForm id={this.state.id} user={this.state.user} updateUser={this.getUser} />)} />
-        <AppliedRoute path="/profile" component={() => (<Profile currentUser={this.state.user} userID={this.state.id} updateUser={this.getUser} />)} />
 
 
         <AppliedRoute path='/sandbox' component={() => (<SandBox userID={this.state.id} updateUser={this.getUser} />)} />
