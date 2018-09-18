aa89480e6bb73f3080a79d334b697b36594c1b70
thegreatsantini <lss13@my.fsu.edu>
2018-09-17 20:44:49 -0700

    fixed auth issues

--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -43,64 +43,66 @@ export default class SignUp extends Component {
     }
 
     render() {
-
-        if (this.props.user) { return (<Redirect to="/" />); }
-
-        return (
-            <div>
-                <br />
-                <h3>Sign up to get started</h3>
-                <br />
-                <Form onSubmit={this.handleSubmit} horizontal>
-                    <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            Name
-                        </Col>
-                        <Col sm={5}>
-                            <FormControl
-                                type="Name"
-                                placeholder="Name"
-                                value={this.state.name}
-                                onChange={this.handleNameChange}
-                            />
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup controlId="formHorizontalEmail">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            Email
-                        </Col>
-                        <Col sm={5}>
-                            <FormControl
-                                type="email"
-                                placeholder="Email"
-                                value={this.state.email}
-                                onChange={this.handleEmailChange}
-                            />
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup controlId="formHorizontalPassword">
-                        <Col componentClass={ControlLabel} sm={2}>
-                            Password
-                        </Col>
-                        <Col sm={5}>
-                            <FormControl
-                                type="password"
-                                placeholder="Password"
-                                value={this.state.password}
-                                onChange={this.handlePasswordChange}
-                            />
-                        </Col>
-                    </FormGroup>
-
-                    <FormGroup>
-                        <Col smOffset={2} sm={10}>
-                            <Button type="submit">Sign in</Button>
-                        </Col>
-                    </FormGroup>
-                </Form>
-            </div>
-        )
+        // console.log('user', this.props.name)
+        if (this.props.id) { return (<Redirect to="/" />); }
+        else {
+            return (
+                <div>
+                    <br />
+                    <h3>Sign up to get started</h3>
+                    <br />
+                    <Form onSubmit={this.handleSubmit} horizontal>
+                        <FormGroup controlId="formHorizontalEmail">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Name
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl
+                                    type="Name"
+                                    placeholder="Name"
+                                    value={this.state.name}
+                                    onChange={this.handleNameChange}
+                                />
+                            </Col>
+                        </FormGroup>
+    
+                        <FormGroup controlId="formHorizontalEmail">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Email
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl
+                                    type="email"
+                                    placeholder="Email"
+                                    value={this.state.email}
+                                    onChange={this.handleEmailChange}
+                                />
+                            </Col>
+                        </FormGroup>
+    
+                        <FormGroup controlId="formHorizontalPassword">
+                            <Col componentClass={ControlLabel} sm={2}>
+                                Password
+                            </Col>
+                            <Col sm={5}>
+                                <FormControl
+                                    type="password"
+                                    placeholder="Password"
+                                    value={this.state.password}
+                                    onChange={this.handlePasswordChange}
+                                />
+                            </Col>
+                        </FormGroup>
+    
+                        <FormGroup>
+                            <Col smOffset={2} sm={10}>
+                                <Button type="submit">Sign in</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+                </div>
+            )
+        }
+        
     }
 }
