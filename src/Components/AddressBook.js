3cc7aaefe25911ef5666a58f2e88ec652e52a40a
thegreatsantini <lss13@my.fsu.edu>
2018-10-03 21:55:19 -0700

    cleanded up addres boo

--- src/Components/AddressBook.js
+++ src/Components/AddressBook.js
@@ -4,9 +4,11 @@ import {
     Table,
     thead,
     tr,
-    Well
+    Button,
+    Glyphicon
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const tableContainer = {
     margin: "0 auto",
@@ -16,7 +18,22 @@ const tableContainer = {
 }
 
 
-export default ({ list }) => {
+export default ({ list, userId, updateUser }) => {
+
+    const handleRemove = async (e, val) => {
+        e.preventDefault()
+        const token = localStorage.getItem('loginToken');
+
+        const response = await axios({
+            method: 'DELETE',
+            url: `${process.env.REACT_APP_DEV_SERVER}/addressbook/api/v1/${userId}/remove/${val}`,
+            headers: { 'Authorization': `Bearer ${token}` },
+        });
+
+        updateUser()
+    }
+
+
     const tableBody = list.map((val, i) => {
         const getName = (name, index) => name.split(" ")[index];
         return (
@@ -25,6 +42,23 @@ export default ({ list }) => {
                 <td>{getName(val.name, 0)}</td>
                 <td>{getName(val.name, 1)}</td>
                 <td>{val.email} </td>
+                <Button bsSize="small">
+                    <Link
+                        to={{
+                            pathname: `/contacts/edit/${val._id}`,
+                            state: {
+                                name: val.name,
+                                email: val.email,
+                                contactId: val._id
+                            }
+                        }}
+                    >
+                        <Glyphicon glyph="pencil" />
+                    </Link>
+                </Button>
+                <Button onClick={(e) => handleRemove(e, val._id)} bsSize="small">
+                    <Glyphicon glyph="trash" />
+                </Button>
             </tr>
         );
     });
@@ -32,19 +66,19 @@ export default ({ list }) => {
     return (
         <div style={tableContainer}>
             {/* <Well> */}
-                <Table responsive striped bordered condensed hover >
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>First</th>
-                            <th>Last</th>
-                            <th>email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {tableBody}
-                    </tbody>
-                </Table>
+            <Table responsive striped bordered condensed hover >
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First</th>
+                        <th>Last</th>
+                        <th>email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableBody}
+                </tbody>
+            </Table>
             {/* </Well> */}
         </div>
     );
