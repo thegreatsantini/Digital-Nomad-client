89c224b13baea2fa29ed840c82be1b7fe98b4a5b
thegreatsantini <lss13@my.fsu.edu>
2018-10-25 11:12:33 -0700

    fixed warnings and began stylings

--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 import SentPostCards from './SentPostCards';
 import { Redirect } from 'react-router-dom'
 
@@ -8,25 +7,14 @@ export default class Profile extends Component {
         super(props);
         this.state = {
             editUser: false,
-
         };
     }
 
     renderProfilePage = () => {
         return (
             <React.Fragment>
-                {/* <Image src={ testProfile } rounded alt='avatar' />
-                < ListGroup >
-                    <ListGroupItem>Name: {this.props.name} </ListGroupItem>
-                    <ListGroupItem>Email: {this.props.email} </ListGroupItem>
-                    <ListGroupItem> 'other stuff' </ListGroupItem>
-                </ListGroup >
-                <Button
-                    bsStyle="info"
-                    onClick={() => this.setState({ editUser: true })}
-                >
-                    Update Info
-                </Button> */}
+                <h2>Sent post cards</h2>
+                <hr />
                 <SentPostCards
                     userId={this.props.id}
                     updateUser={this.props.updateUser}
