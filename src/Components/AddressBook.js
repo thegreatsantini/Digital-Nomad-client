688c97d9091b0e575d91c04da8001f45cde8aa0a
thegreatsantini <lss13@my.fsu.edu>
2018-12-16 17:05:46 -0800

    added alerts for user actions

--- src/Components/AddressBook.js
+++ src/Components/AddressBook.js
@@ -1,85 +1,74 @@
-import React from 'react';
-import {
-    th,
-    Table,
-    thead,
-    tr,
-    Button,
-    Glyphicon
-} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { th, Table, thead, tr, Button, Glyphicon } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
+import axios from "axios";
 
 const tableContainer = {
-    margin: "0 auto",
-    padding: "10px 12%",
-    overflow: "scroll",
-    height: "300px"
-}
+  margin: "0 auto",
+  padding: "10px 12%",
+  overflow: "scroll",
+  height: "300px"
+};
 
+export default ({ list, userId, updateUser, toggleAlert }) => {
+  const handleRemove = async (e, val) => {
+    e.preventDefault();
+    const token = localStorage.getItem("loginToken");
 
-export default ({ list, userId, updateUser }) => {
-
-    const handleRemove = async (e, val) => {
-        e.preventDefault()
-        const token = localStorage.getItem('loginToken');
-
-        await axios({
-            method: 'DELETE',
-            url: `${process.env.REACT_APP_DEV_SERVER}/addressbook/api/v1/${userId}/remove/${val}`,
-            headers: { 'Authorization': `Bearer ${token}` },
-        });
-
-        updateUser()
-    }
-
-
-    const tableBody = list.map((val, i) => {
-        const getName = (name, index) => name.split(" ")[index];
-        return (
-            <tr>
-                {/* <td>{i + 1}</td> */}
-                <td>{getName(val.name, 0)}</td>
-                <td>{getName(val.name, 1)}</td>
-                <td>{val.email} </td>
-                <Button bsSize="small">
-                    <Link
-                        to={{
-                            pathname: `/contacts/edit/${val._id}`,
-                            state: {
-                                name: val.name,
-                                email: val.email,
-                                contactId: val._id
-                            }
-                        }}
-                    >
-                        <Glyphicon glyph="pencil" />
-                    </Link>
-                </Button>
-                <Button onClick={(e) => handleRemove(e, val._id)} bsSize="small">
-                    <Glyphicon glyph="trash" />
-                </Button>
-            </tr>
-        );
+    await axios({
+      method: "DELETE",
+      url: `${
+        process.env.REACT_APP_DEV_SERVER
+      }/addressbook/api/v1/${userId}/remove/${val}`,
+      headers: { Authorization: `Bearer ${token}` }
     });
+    toggleAlert('removed')
+    updateUser();
+  };
 
+  const tableBody = list.map((val, i) => {
+    const getName = (name, index) => name.split(" ")[index];
     return (
-        <div style={tableContainer}>
-            {/* <Well> */}
-            <Table responsive striped bordered condensed hover >
-                <thead>
-                    <tr>
-                        {/* <th>#</th> */}
-                        <th>First</th>
-                        <th>Last</th>
-                        <th>email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableBody}
-                </tbody>
-            </Table>
-            {/* </Well> */}
-        </div>
+      <tr key={i}>
+        {/* <td>{i + 1}</td> */}
+        <td>{getName(val.name, 0)}</td>
+        <td>{getName(val.name, 1)}</td>
+        <td>{val.email} </td>
+        <LinkContainer
+          to={{
+            pathname: `/contacts/edit/${val._id}`,
+            state: {
+              name: val.name,
+              email: val.email,
+              contactId: val._id
+            }
+          }}
+        >
+          <Button bsSize="small">
+            <Glyphicon glyph="pencil" />
+          </Button>
+        </LinkContainer>
+        <Button onClick={e => handleRemove(e, val._id)} bsSize="small">
+          <Glyphicon glyph="trash" />
+        </Button>
+      </tr>
     );
-}
+  });
+
+  return (
+    <div style={tableContainer}>
+      <Table responsive striped bordered condensed hover>
+        <thead>
+          <tr>
+            {/* <th>#</th> */}
+            <th>First</th>
+            <th>Last</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>{tableBody}</tbody>
+      </Table>
+    </div>
+  );
+};
