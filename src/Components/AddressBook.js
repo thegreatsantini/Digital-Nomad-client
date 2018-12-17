dc5b2f35b6ab9084a04271446248cba5082b9ac7
thegreatsantini <lss13@my.fsu.edu>
2018-12-16 17:23:24 -0800

    removed bootstar

--- src/Components/AddressBook.js
+++ src/Components/AddressBook.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { th, Table, thead, tr, Button, Glyphicon } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { LinkContainer } from "react-router-bootstrap";
 import axios from "axios";
 
 const tableContainer = {
